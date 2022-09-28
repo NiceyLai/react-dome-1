76e050e930df943a85fe5fb06005b8e0efc039df
nicey <laisi1127@163.com>
2022-09-28 17:53:01 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
-import React, { useState } from "react";
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       n:1
-//     }
-//   }
-
-//   onClick = () => {
-//     this.setState((state) => ({
-//       n: state.n+1
-//     }))
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         App
-//         <div>
-//           {this.state.n}
-//           <button onClick={this.onClick}>+1</button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+import React, { useState ,useEffect} from "react";
 
 const App = (props) => {
   const [n, setN] = useState(0);
   const onClick = () => {
     setN(n + 1);
   };
+
+  useEffect(() => {
+    console.log('11111111t');
+  },[])
   return (
     <div>
       {n}
